c04b9e073aa4727268c8637fa4dba5add8c46c7d
Troy Griffin <troygrid@amazon.co.uk>
2023-10-22 22:55:53 +0100

    added bert and metric storage

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import json
 import pandas as pd
 import openpyxl
 
-
+import tensorflow_hub as hub
+import tokenization_bert
 import tensorflow as tf
 # helps in text preprocessing
 from keras.preprocessing.sequence import pad_sequences
@@ -22,12 +23,22 @@ from keras.callbacks import EarlyStopping
 import torch
 import torch.nn as nn
 
+from tqdm.notebook import tqdm
+
+from transformers import BertTokenizer, get_linear_schedule_with_warmup, BertForSequenceClassification
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
 from sklearn.metrics import classification_report
+import transformers
+
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from torch.optim import AdamW
 import transformers
 from transformers import AutoModel, BertTokenizerFast
 
-# specify GPU
-device = torch.device("cuda")
+
+
+from sklearn.utils.class_weight import compute_class_weight
 
 # split data into train and test set
 from sklearn.model_selection import train_test_split
@@ -63,9 +74,9 @@ from nltk.stem import WordNetLemmatizer
 
 import matplotlib.pyplot as plt
 
-nltk.download ('wordnet')
+"""nltk.download ('wordnet')
 nltk.download("punkt")
-nltk.download("stopwords")
+nltk.download("stopwords")"""
 
 py_lemmatizer = WordNetLemmatizer ()
 stop = stopwords.words("english")
@@ -155,10 +166,67 @@ def get_subjects(entries: list) -> list:
       subjects.append(subject)
     return subjects
 
+def get_metrics(df, report, model_name, t):
+  row = {'Model': model_name, 'Class': 'total', 'Parameter/Metric': 'Training Time', 'Value': round(t, 2)}
+  df = df._append(row, ignore_index=True)
+  for label in report:
+    if label == 'accuracy':
+      row = {'Model': model_name, 'Class': 'total', 'Parameter/Metric': label.title(), 'Value': round(report[label], 2)}
+      df = df._append(row, ignore_index=True)
+      continue
+    for metric in report[label]:
+      if metric == 'support':
+        continue
+      row = {'Model': model_name, 'Class': label.title(), 'Parameter/Metric': metric.title(), 'Value': round(report[label][metric], 2)}
+      df = df._append(row, ignore_index=True)
+  return df
+
+def bert_encode(texts, tokenizer, max_len=512):
+  all_tokens = []
+  all_masks = []
+  all_segments = []
+  
+  for text in texts:
+      text = tokenizer.tokenize(text)
+          
+      text = text[:max_len-2]
+      input_sequence = ["[CLS]"] + text + ["[SEP]"]
+      pad_len = max_len - len(input_sequence)
+      
+      tokens = tokenizer.convert_tokens_to_ids(input_sequence) + [0] * pad_len
+      pad_masks = [1] * len(input_sequence) + [0] * pad_len
+      segment_ids = [0] * max_len
+      
+      all_tokens.append(tokens)
+      all_masks.append(pad_masks)
+      all_segments.append(segment_ids)
+    
+  return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
+
+
+def build_model(bert_layer, max_len=512):
+  input_word_ids = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
+  input_mask = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="input_mask")
+  segment_ids = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="segment_ids")
+
+  pooled_output, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
+  clf_output = sequence_output[:, 0, :]
+  net = tf.keras.layers.Dense(64, activation='relu')(clf_output)
+  net = tf.keras.layers.Dropout(0.2)(net)
+  net = tf.keras.layers.Dense(32, activation='relu')(net)
+  net = tf.keras.layers.Dropout(0.2)(net)
+  out = tf.keras.layers.Dense(3, activation='softmax')(net)
+  
+  model = tf.keras.models.Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=out)
+  model.compile(tf.keras.optimizers.Adam(lr=1e-5), loss='categorical_crossentropy', metrics=['accuracy'])
+  
+  return model
+
+
 
 if __name__ == "__main__":
-  """
-  with open('scraping_scripts/lexicon.json', 'r') as f:
+  
+  """with open('scraping_scripts/lexicon.json', 'r') as f:
     data = json.load(f)
   df_lexicon = pd.DataFrame(data)
 
@@ -182,46 +250,25 @@ if __name__ == "__main__":
   df_data["combined"] = df_data["Title"] + " " + df_data["Abstract"] + " " + df_data["Author Keywords"] + " " + df_data["Index Keywords"]
   df_data["subjects"] = get_subjects(df_data["combined"].values.flatten().tolist())
 
-  remove_words = ['tissue']
-  for string in remove_words:
-    df_data.drop(df_data[df_data['combined'].str.contains(string)].index, inplace = True)
-
   df_data = df_data[df_data['subjects'].map(len) < 2]
   df_data['subjects'] = [','.join(map(str, l)) for l in df_data['subjects']]
-  
-  df_mech = df_data[(df_data["subjects"] == 'mechanical')].iloc[0:360, :]
-  df_mat = df_data[(df_data["subjects"] == 'material')].iloc[0:360, :]
-  df_aero = df_data[(df_data["subjects"] == 'aeronautical')].iloc[0:360, :]
-
-  path = "processedstep2.xlsx"
+  #Save output here to show all single subject entries
+  path = "processedstep1.xlsx"
   df_data.to_excel(path, sheet_name="Data", index=False)
+  #df_data = pd.read_excel(path)
 
-  df_balanced_data = pd.concat([df_mech, df_mat, df_aero])
-
-  del df_balanced_data['Source title']
-  del df_balanced_data['Author Keywords']
-  del df_balanced_data['Index Keywords']
-  del df_balanced_data['combined'] 
-  df_balanced_data['text'] = df_balanced_data['Title'] + " " + df_balanced_data['Abstract']
-  del df_balanced_data['Title']
-  del df_balanced_data['Abstract']
-  """
-  # Write data to excel output file
-  path = "processed.xlsx"
-  #df_balanced_data.to_excel(path, sheet_name="Data", index=False)
-  df_balanced_data = pd.read_excel(path)
-
-  # Show class distribution and word distribution
+  # Show class distribution and word distribution before balancing
+  df_data['text'] = df_data['Title'] + " " + df_data['Abstract']
   labels = ['mechanical', 'aeronautical', 'material']
   plt.figure(figsize=(10,4))
   plt.ylabel("Occurence")
-  df_balanced_data.subjects.value_counts().plot(kind='bar')
+  df_data.subjects.value_counts().plot(kind='bar')
   
 
   words = []
-  
-  entries = df_balanced_data['text'].values.flatten().tolist()
-  for entry in entries:
+  entries = df_data['text'].values.flatten().tolist()
+  for i, entry in enumerate(entries):
+    print(i)
     sentence = entry.split()
     for word in sentence:
       words.insert(0, word)
@@ -229,30 +276,78 @@ if __name__ == "__main__":
   plt.figure(figsize=(10,4))
   plt.ylabel("Occurence")
   words.value_counts()[:40].plot(kind='bar')
+
+  remove_entries = ['tissue']
+  for string in remove_entries:
+    df_data.drop(df_data[df_data['combined'].str.contains(string)].index, inplace = True)
+
+  df_aero = df_data[(df_data["subjects"] == 'aeronautical')]
+  length = len(df_aero)
+  df_mech = df_data[(df_data["subjects"] == 'mechanical')].iloc[0:length,:]
+  df_mat = df_data[(df_data["subjects"] == 'material')].iloc[0:length,:]
+  df_data = pd.concat([df_mat, df_mech, df_aero])
+  
+
+  bias_words = ['engineering','material','mechanical','aeronautical']
+  
+  for string in bias_words:
+    df_data['Title'] = df_data['Title'].replace(string, '', regex = True)
+    df_data['Abstract'] = df_data['Abstract'].replace(string, '', regex = True)
+  
+  path = "processedstep2.xlsx"
+  df_data.to_excel(path, sheet_name="Data", index=False)
+
+  del df_data['Source title']
+  del df_data['Author Keywords']
+  del df_data['Index Keywords']
+  del df_data['combined'] 
+  df_data['text'] = df_data['Title'] + " " + df_data['Abstract']
+  del df_data['Title']
+  del df_data['Abstract']"""
+  
+  # Write data to excel output file
+  path = "processedstep3.xlsx"
+  #df_data.to_excel(path, sheet_name="Data", index=False)
+  df_data = pd.read_excel(path)
+
+  """# Show class distribution and word distribution after balancing
+  labels = ['mechanical', 'aeronautical', 'material']
+  plt.figure(figsize=(10,4))
+  plt.ylabel("Occurence")
+  df_data.subjects.value_counts().plot(kind='bar')
+  
+
+  words = []
+  entries = df_data['text'].values.flatten().tolist()
+  for entry in entries:
+    sentence = entry.split()
+    for word in sentence:
+      words.insert(0, word)
+  words = pd.Series(words)
+  plt.figure(figsize=(10,4))
+  plt.ylabel("Occurence")
+  words.value_counts()[:40].plot(kind='bar')"""
   
 
   # Shuffle dataframe entries randomly
-  df_balanced_data.sample(frac = 1)
+  df_data.sample(frac = 1)
 
-  df_mech = df_balanced_data[(df_balanced_data["subjects"] == 'mechanical')]
-  df_mat = df_balanced_data[(df_balanced_data["subjects"] == 'material')]
-  df_aero = df_balanced_data[(df_balanced_data["subjects"] == 'aeronautical')]
+  df_mech = df_data[(df_data["subjects"] == 'mechanical')]
+  df_mat = df_data[(df_data["subjects"] == 'material')]
+  df_aero = df_data[(df_data["subjects"] == 'aeronautical')]
 
   df_full = pd.concat([df_mech, df_mat, df_aero])
-  df_train = pd.concat([df_mech.iloc[0:287], df_mat.iloc[0:287], df_aero.iloc[0:287]])
-  df_val = pd.concat([df_mech.iloc[287:324], df_mat.iloc[287:324], df_aero.iloc[287:324]])
-  df_trainval = pd.concat([df_mech.iloc[0:324], df_mat.iloc[0:324], df_aero.iloc[0:324]])
-  df_test = pd.concat([df_mech.iloc[324:360], df_mat.iloc[324:360], df_aero.iloc[324:360]])
-  df_metrics = pd.DataFrame({'Model': [], 'Parameter/Metric': [], 'Value':[]})
+  df_metrics = pd.DataFrame({'Model': [],'Class': [], 'Parameter/Metric': [], 'Value':[]})
 
   """Process for model trainings, train data and plot learning curve, save hyperparametrs in a table, plot classifcation matrix (multiclass and binary), save evaluation metrics in table"""
   
   # Train & Test Naive Bayes
+  t0 = time.time()
   x = df_full.text
   y = df_full.subjects
   x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state = 42)
 
-  """nb = Pipeline([('vect', CountVectorizer()),
+  nb = Pipeline([('vect', CountVectorizer()),
                ('tfidf', TfidfTransformer()),
                ('clf', MultinomialNB()),
               ])
@@ -261,11 +356,21 @@ if __name__ == "__main__":
 
   y_pred = nb.predict(x_test)
 
+  t1 = time.time()
+
+  t_train = t1 - t0
+
   print('accuracy %s' % accuracy_score(y_pred, y_test))
-  print(classification_report(y_test, y_pred,target_names=['mechanical','material','aeronautical']))
+  print(classification_report(y_test, y_pred))
+  report = classification_report(y_test, y_pred,target_names=['mechanical','material','aeronautical'], output_dict=True)
   
+  df_metrics = get_metrics(df_metrics, report, 'MNB', t_train)
+  print(df_metrics)
+
   model_plot(nb, x_train, y_train)
 
+
+  """
   # Train & Test SVM
   sgd = Pipeline([('vect', CountVectorizer()),
                 ('tfidf', TfidfTransformer()),
@@ -342,7 +447,7 @@ if __name__ == "__main__":
   rnn.add(SpatialDropout1D(0.2))
   rnn.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
   rnn.add(Dense(3, activation='softmax'))
-  rnn.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+  rnn.compile(loss='categorical_crossentropy', optimizer='adamw', metrics=['accuracy'])
 
   epochs = 50
   batch_size = 64
@@ -364,12 +469,66 @@ if __name__ == "__main__":
   plt.plot(history.history['accuracy'], label='train')
   plt.plot(history.history['val_accuracy'], label='test')
   plt.legend()
-  """
+  
 
   # Train & Test BERT
+  possible_labels = df_full.subjects.unique()
 
+  label_dict = {}
+  for index, possible_label in enumerate(possible_labels):
+    label_dict[possible_label] = index
+  df = df_full
+  df['subjects'] = df.subjects.replace(label_dict)
 
+  x_train, x_temp, y_train, y_temp = train_test_split(df.text, df.subjects, random_state=42, test_size=0.2)
+
+  x_val, x_test, y_val, y_test = train_test_split(x_temp, y_temp, random_state=42, test_size=0.5)
+  
+  module_url = 'https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2'
+  bert_layer = hub.KerasLayer(module_url, trainable=True)
+
+  vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
+  do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
+  tokenizer = tokenization_bert.FullTokenizer(vocab_file, do_lower_case)
+
+  max_len = 512
+  train_input = bert_encode(x_train, tokenizer, max_len=max_len)
+  test_input = bert_encode(x_test, tokenizer, max_len=max_len)
+  train_labels = tf.keras.utils.to_categorical(y_train, num_classes=3)
+  test_labels = tf.keras.utils.to_categorical(y_test, num_classes=3)
+  
+  model = build_model(bert_layer, max_len=max_len)
+  model.summary()
+  
+
+  checkpoint = tf.keras.callbacks.ModelCheckpoint('model.h5', monitor='val_accuracy', save_best_only=True, verbose=1)
+  earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=15, verbose=1)
+
+  epochs = 50
+  batch_size = 16
+
+  train_history = model.fit(train_input, train_labels,epochs = epochs, batch_size=batch_size, validation_split=0.1111111,callbacks=[checkpoint, earlystopping],verbose=1)
+  
+  #Predictions for the test data:
+
+  model.load_weights('model.h5')
+
+  test_pred = model.predict(test_input)
+
+  test_pred = np.argmax(test_pred, axis=1)
+
+  print(classification_report(y_test, test_pred,target_names=['mechanical','material','aeronautical']))
+  plt.figure()
+  plt.title('Loss')
+  plt.plot(history.history['loss'], label='train')
+  plt.plot(history.history['val_loss'], label='test')
+  plt.legend()
+  
+  plt.figure()
+  plt.title('Accuracy')
+  plt.plot(train_history.history['accuracy'], label='train')
+  plt.plot(train_history.history['val_accuracy'], label='test')
+  plt.legend()
+  """
   # Compare Metrics (Calculate overall score based on metric priorities)
-
-
   plt.show()
